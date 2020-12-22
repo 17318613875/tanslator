f80e838274d2a023322d7eff77a1c7b51222db30
彭斌斌 <30690562+17318613875@users.noreply.github.com>
2020-12-22 14:16:44 +0800

    docs(babel.config.js): 修改 应webpack init 造成 jest ES6 报错的问题

--- packages/RxPromise/babel.config.js
+++ packages/RxPromise/babel.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  presets: [
-    ["@babel/preset-env", { modules: false, targets: { node: "current" } }],
-  ],
+  presets: [["@babel/preset-env", { targets: { node: "current" } }]],
   plugins: ["syntax-dynamic-import", "@babel/plugin-proposal-class-properties"],
 };
